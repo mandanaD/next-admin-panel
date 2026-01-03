f609a0466f6bfd5f2290862df0b22ce196355bcf
Mandana Dashti <dmandana59@gmail.com>
2026-01-03 19:21:01 +0330

    style: add sidebar

--- app/(panel)/layout.tsx
+++ app/(panel)/layout.tsx
@@ -1,11 +1,15 @@
+import {Sidebar} from "@/src/components/layout/Sidebar/Sidebar";
+
 export default function PanelLayout({
                                         children,
                                     }: {
     children: React.ReactNode
 }) {
     return (
-        <div className="min-h-screen">
-            <div className="flex flex-col flex-1 overflow-hidden">
+        <div className="h-screen">
+
+            <div className="flex flex-1 overflow-hidden">
+                <Sidebar/>
                 <main className="flex-1 overflow-y-auto p-6">
                     {children}
                 </main>
